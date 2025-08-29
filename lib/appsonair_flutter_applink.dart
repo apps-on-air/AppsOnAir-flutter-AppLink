e433d33377e5f2e52449842d1eaa8859dee653d7
suryakant-dubey-us <suryakant.dubey@logicwind.com>
2025-08-29 19:37:12 +0530

    added referral listner and deprecated old referral method

--- lib/appsonair_flutter_applink.dart
+++ lib/appsonair_flutter_applink.dart
@@ -10,12 +10,24 @@ class AppsonairFlutterApplink {
   }
 
   ///Return the referral link which user click for installing the application
+  @Deprecated('Use getReferralInfo() instead')
   Future<Map<String, dynamic>?> getReferralDetails() async {
     return await AppsonairFlutterApplinkPlatform.instance.getReferralDetails();
+  }
+
+  ///Return the referral link which user click for installing the application
+  ///It will wait till untill referral data reflected
+  Future<Map<String, dynamic>?> getReferralInfo() async {
+    return await AppsonairFlutterApplinkPlatform.instance.getReferralInfo();
   }
 
   ///Initialize the applink service in your application
   Stream<Map<String, dynamic>?> initializeAppLink() {
     return AppsonairFlutterApplinkPlatform.instance.initializeAppLink();
+  }
+
+  ///Provide the referral detail once app is launched after install for first time.
+  Stream<Map<String, dynamic>?> onReferralLinkDetected() {
+    return AppsonairFlutterApplinkPlatform.instance.onReferralLinkDetected();
   }
 }
